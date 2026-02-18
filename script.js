237e97c5b6d6f2da9b3e0f29396636dac11311d9
redmariogamer <138834245+redmariogamer@users.noreply.github.com>
2026-02-18 11:31:51 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+/* slide images*/
+document.addEventListener("DOMContentLoaded", function() {
+    const submitQuiz = document.getElementById("submitQuiz");
+    const quizResult = document.getElementById("quizResult");
+
+    
+    if (submitQuiz && quizResult) {
+    submitQuiz.addEventListener("click", function() {
+
+        let score = 0;
+
+        const answers = {
+            q1: "Super Mario",
+            q2: "True",
+            q3: "Pizza",
+            q4: "Cat",
+            q5: "Video Games",
+            q6: "Golden State Warriors",
+            q7: "Super Mario Bros. Movie",
+            q8: "Avatar: The Last Airbender",
+            q9: "Behavior Technician",
+            q10: "YES!",
+        };
+        
+        for (let question in answers) {
+            const selected = document.querySelector(`input[name="${question}"]:checked`);
+            if (selected && selected.value === answers[question]) {
+                score++;
+            }
+        }
+        quizResult.textContent = `You scored ${score} out of 10!`;
+        quizResult.style.fontWeight = "bold";
+        quizResult.style.marginTop = "10px";
+        quizResult.style.color = "green";
+    });
+}
+});
