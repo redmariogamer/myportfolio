5817d5f6d7e5fd6b6b815c89b8e9c1563a3e7a74
redmariogamer <138834245+redmariogamer@users.noreply.github.com>
2026-05-26 22:12:16 -0700

    Add files via upload

--- slides.js
+++ slides.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const images = ['Slideshow_images/image1.jpg','Slideshow_images/image2.jpg','Slideshow_images/image3.jpeg','Slideshow_images/image4.jpg','Slideshow_images/image5.jpg','Slideshow_images/image6.jpg', 'Slideshow_images/image7.jpg', 'Slideshow_images/image8.jpg', 'Slideshow_images/image9.jpg', 'Slideshow_images/image10.jpg'];
+    const images = ['Slideshow_images/image1.jpg','Slideshow_images/image2.jpg','Slideshow_images/image3.jpg','Slideshow_images/image4.jpg','Slideshow_images/image5.jpg','Slideshow_images/image6.jpg', 'Slideshow_images/image7.jpg', 'Slideshow_images/image8.jpg', 'Slideshow_images/image9.jpg', 'Slideshow_images/image10.jpg'];
     let currentIndex = 0;
 
     const previousBtn = document.getElementById('previousBtn');
